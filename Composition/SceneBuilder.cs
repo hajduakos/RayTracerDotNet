535113bf286378febfeca1410cba507edee0bc8a
Hajdu Ákos <hajdua@mit.bme.hu>
2019-09-29 21:55:00 +0200

    Load scenes from xml

---
+++ Composition/SceneBuilder.cs
@@ -0,0 +1,94 @@
+﻿using RayTracer.Common;
+using RayTracer.Objects;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml;
+
+namespace RayTracer.Composition
+{
+    public class SceneBuilder
+    {
+        private static NumberFormatInfo nfi = CultureInfo.InvariantCulture.NumberFormat;
+        public static Scene FromXML(string filename)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename);
+
+            int scenew = 1080;
+            int sceneh = 720;
+            Vec3 cameye = new Vec3(1, 1, 1);
+            Vec3 camlookat = new Vec3(0, 0, 0);
+            float camhfov = 50;
+
+            XmlNode sceneNode = doc.ChildNodes[0];
+            if (sceneNode.Attributes != null)
+            {
+                if (sceneNode.Attributes["w"] != null) scenew = Convert.ToInt32(sceneNode.Attributes["w"].Value);
+                if (sceneNode.Attributes["h"] != null) sceneh = Convert.ToInt32(sceneNode.Attributes["h"].Value);
+                if (sceneNode.Attributes["cameye"] != null) cameye = Vec3FromString(sceneNode.Attributes["cameye"].Value);
+                if (sceneNode.Attributes["camlookat"] != null) camlookat = Vec3FromString(sceneNode.Attributes["camlookat"].Value);
+                if (sceneNode.Attributes["camhfov"] != null) camhfov = Convert.ToSingle(sceneNode.Attributes["camhfov"].Value, nfi);
+            }
+            Scene scene = new Scene(scenew, sceneh, new Camera(cameye, camlookat, camhfov * MathF.PI / 180, scenew, sceneh));
+            Dictionary<string, Material> materials = new Dictionary<string, Material>();
+
+            foreach (XmlNode node in sceneNode.ChildNodes)
+            {
+                if (node.Name == "material")
+                {
+                    string id = node.Attributes["id"].Value;
+                    Color ambient = node.Attributes["ambient"] == null ?
+                        new Color(0, 0, 0) : ColorFromString(node.Attributes["ambient"].Value);
+                    Color diffuse = node.Attributes["diffuse"] == null ?
+                        new Color(0, 0, 0) : ColorFromString(node.Attributes["diffuse"].Value);
+                    Color specular = node.Attributes["specular"] == null ?
+                        new Color(0, 0, 0) : ColorFromString(node.Attributes["specular"].Value);
+                    float shine = node.Attributes["shine"] == null ? 0 : Convert.ToSingle(node.Attributes["shine"].Value, nfi);
+                    materials.Add(id, new Material(ambient, diffuse, specular, shine));
+                }
+                else if (node.Name == "light")
+                {
+                    scene.AddLight(new PointLight(
+                        Vec3FromString(node.Attributes["pos"].Value),
+                        ColorFromString(node.Attributes["lum"].Value)));
+                }
+                else if (node.Name == "planexy")
+                {
+                    scene.AddObject(new PlaneXY(materials[node.Attributes["material"].Value]));
+                }
+                else if (node.Name == "sphere")
+                {
+                    scene.AddObject(new Sphere(
+                        Vec3FromString(node.Attributes["center"].Value),
+                        Convert.ToSingle(node.Attributes["radius"].Value, nfi),
+                        materials[node.Attributes["material"].Value]
+                        ));
+                }
+                else
+                {
+                    throw new Exception("Unknown node: " + node.Name);
+                }
+            }
+            
+            return scene;
+        }
+
+        private static Vec3 Vec3FromString(string s)
+        {
+            string[] values = s.Split(" ");
+            return new Vec3(
+                Convert.ToSingle(values[0], nfi),
+                Convert.ToSingle(values[1], nfi),
+                Convert.ToSingle(values[2], nfi));
+        }
+        private static Color ColorFromString(string s)
+        {
+            string[] values = s.Split(" ");
+            return new Color(
+                Convert.ToSingle(values[0], nfi),
+                Convert.ToSingle(values[1], nfi),
+                Convert.ToSingle(values[2], nfi));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using RayTracer.Common;
-using RayTracer.Composition;
-using RayTracer.Objects;
-using System;
-using System.Diagnostics;
+﻿using RayTracer.Composition;
 
 namespace RayTracer
 {
@@ -10,17 +6,11 @@ namespace RayTracer
     {
         static void Main(string[] args)
         {
-            Scene scene = new Scene(1280, 720, new Camera(new Vec3(7, 0, 2.5f), new Vec3(0, 0, 1), 50 / 180f * MathF.PI, 1280, 720));
-            scene.AddLight(new PointLight(new Vec3(40, 40, 40), new Color(6400, 6400, 6400)));
-            scene.AddObject(new PlaneXY(new Material(new Color(170f / 2550, 117f / 2550, 0), new Color(170f / 255, 117f / 255, 0), new Color(), 0)));
-            scene.AddObject(new Sphere(new Vec3(0, 0, 1), 1, new Material(new Color(0.1f, 0, 0), new Color(1, 0, 0), new Color(), 0)));
-            scene.AddObject(new Sphere(new Vec3(0, -2, 1), 1, new Material(new Color(0, 0.1f, 0), new Color(0, .5f, 0), new Color(0, .5f, 0), 5)));
-            scene.AddObject(new Sphere(new Vec3(0, 2, 1), 1, new Material(new Color(0, 0, 0.1f), new Color(), new Color(0, 0, 1), 10)));
-            Stopwatch sw = Stopwatch.StartNew();
-            System.Drawing.Bitmap bmp = scene.Render().ToBitmap();
-            sw.Stop();
-            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
-            bmp.Save("test.bmp");
+            Scene scene = SceneBuilder.FromXML(args[0]);
+            using (System.Drawing.Bitmap bmp = scene.Render().ToBitmap())
+            {
+                bmp.Save(args[1]);
+            }
         }
     }
 }
