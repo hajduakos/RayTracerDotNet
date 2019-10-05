e03c867e8b34a9616769ca4b14500c69896bfc5e
Akos Hajdu <hajdua@mit.bme.hu>
2019-10-05 10:17:55 +0200

    Add timeout

--- render-all.sh
+++ render-all.sh
@@ -1,1 +1,4 @@
-for f in Scenes/*.xml; do dotnet bin/Release/netcoreapp3.0/RayTracer.dll $f "$f.png"; done
+for f in Scenes/*.xml
+do
+    timeout $1 dotnet bin/Release/netcoreapp3.0/RayTracer.dll $f "$f.png"
+done
