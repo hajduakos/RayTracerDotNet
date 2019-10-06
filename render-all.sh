c82abf150c954dfd2dca0713009d617a3bcbbb20
Hajdu Ákos <hajdua@mit.bme.hu>
2019-10-06 13:07:19 +0200

    Measure time

--- render-all.sh
+++ render-all.sh
@@ -1,5 +1,6 @@
 for f in Scenes/*.xml
 do
     echo "Rendering $f with $1 s timeout"
-    timeout $1 dotnet bin/Release/netcoreapp3.0/RayTracer.dll $f "$f.png"
+    time timeout $1 dotnet bin/Release/netcoreapp3.0/RayTracer.dll $f "$f.png"
+    echo "-----"
 done
