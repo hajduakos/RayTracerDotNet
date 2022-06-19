7c0984f9d8259497de4fdb2630b18351add91bb6
hajduakos <sonic18291@hotmail.com>
2022-06-19 13:23:38 +0100

    Add timeout and render on Win

--- render-all.bat
+++ render-all.bat
@@ -1,4 +1,4 @@
 del Scenes\*.png
 FOR %%F IN (Scenes\*.xml) DO (
-    bin\Release\net6.0\RayTracer.exe %%F
+    ScriptRunner.exe -appvscript bin\Release\net6.0\RayTracer.exe %%F -appvscriptrunnerparameters -wait -timeout=%1
 )
