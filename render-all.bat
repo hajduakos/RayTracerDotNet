045401b257d1fa0e2f6af29e7a39befd7af2068d
hajduakos <sonic18291@hotmail.com>
2022-06-21 00:08:10 +0100

    Move project into own folder

--- render-all.bat
+++ render-all.bat
@@ -1,4 +1,4 @@
 del Scenes\*.png
 FOR %%F IN (Scenes\*.xml) DO (
-    ScriptRunner.exe -appvscript bin\Release\net6.0\RayTracer.exe %%F -appvscriptrunnerparameters -wait -timeout=%1
+    ScriptRunner.exe -appvscript RayTracer\bin\Release\net6.0\RayTracer.exe %%F -appvscriptrunnerparameters -wait -timeout=%1
 )
--- render-all.sh
+++ render-all.sh
@@ -2,6 +2,6 @@ rm Scenes/*.png
 for f in Scenes/*.xml
 do
     echo "Rendering $f with $1 s timeout"
-    time timeout $1 dotnet bin/Release/net6.0/RayTracer.dll $f
+    time timeout $1 dotnet RayTracer/bin/Release/net6.0/RayTracer.dll $f
     echo "-----"
 done
