a5427aa278cb6315a36ffd12fe91bfdbafb259f6
hajduakos <sonic18291@hotmail.com>
2024-10-30 23:45:26 +0000

    Update to .NET 8.0

--- render-all.bat
+++ render-all.bat
@@ -1,4 +1,4 @@
 del Scenes\*.bmp
 FOR %%F IN (Scenes\*.xml) DO (
-    ScriptRunner.exe -appvscript RayTracer\bin\Release\net6.0\RayTracer.exe %%F -appvscriptrunnerparameters -wait -timeout=%1
+    ScriptRunner.exe -appvscript RayTracer\bin\Release\net8.0\RayTracer.exe %%F -appvscriptrunnerparameters -wait -timeout=%1
 )
